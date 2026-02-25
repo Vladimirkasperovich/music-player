053dc433782e7aaf2e1c52d2ae98adc5dd7fbfc4
vovak <vova.kasper.boy@gmail.com>
2026-02-25 16:57:31 +0300

    feat: test

--- src/entities/playlists/api/playlists.api.ts
+++ src/entities/playlists/api/playlists.api.ts
@@ -1,7 +1,6 @@
 import { baseApi } from '@/shared/api';
 import type { GetPlaylistsQueryParams, GetPlaylistsResponse } from '@/entities/playlists';
 
-
 export const playlistApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     fetchPlaylists: build.query<GetPlaylistsResponse, GetPlaylistsQueryParams>({
