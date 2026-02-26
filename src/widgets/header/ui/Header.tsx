b340d56edc8c65c01e910a25f16fff30bc9ded96
vovak <vova.kasper.boy@gmail.com>
2026-02-26 14:03:12 +0300

    feat: add avatar for header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,7 +1,11 @@
+import avatar from '../icons/avatar.png';
 export const Header = () => {
   return (
-    <header className="h-20 bg-[#000000] text-center">
-      <h1 className="text-white">This is header</h1>
+    <header className="h-20 bg-[#000000] flex justify-end">
+      <div className="text-white pt-5 pb-5 pr-10 flex items-center gap-3">
+        <img src={avatar} alt="avatar" />
+        <span>Dima Kuzyberdin</span>
+      </div>
     </header>
   );
 };
