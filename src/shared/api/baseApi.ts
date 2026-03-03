e114cd936d3ef68451869d9bd89f77728827e938
vovak <vova.kasper.boy@gmail.com>
2026-03-03 17:23:36 +0300

    feat: test-deploy

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { ACCESS_TOKEN, API_KEY, BASE_URL } from '@/shared/config';
+import { ACCESS_TOKEN, BASE_URL } from '@/shared/config';
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
-    headers: { 'API-KEY': API_KEY },
+    headers: { 'API-KEY': import.meta.env.VITE_API_KEY },
     prepareHeaders: (headers) => {
       headers.set('Authorisation', `Bearer ${ACCESS_TOKEN}`);
       return headers;
