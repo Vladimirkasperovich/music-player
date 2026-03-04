6c5ca36a92e774df06f12d6912be61feb6983706
vovak <vova.kasper.boy@gmail.com>
2026-03-04 11:09:02 +0300

    feat: change headers

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { ACCESS_TOKEN, BASE_URL } from '@/shared/config';
+import { ACCESS_TOKEN, BASE_URL, API_KEY } from '@/shared/config';
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
-    headers: { 'API-KEY': import.meta.env.VITE_API_KEY },
     prepareHeaders: (headers) => {
-      headers.set('Authorisation', `Bearer ${ACCESS_TOKEN}`);
+      headers.set('API-KEY', API_KEY);
+      headers.set('Authorization', `Bearer ${ACCESS_TOKEN}`);
       return headers;
     },
   }),
