0891113f01163748fd0fb520c08c6a5e437fc3f5
vovak <vova.kasper.boy@gmail.com>
2026-03-09 14:16:57 +0300

    feat:finish dropdown position

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,54 +1,51 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router';
 import { DROPDOWN_ITEMS } from '../model/DROPDOWN_ITEMS';
 import avatar from '../icons/avatar.svg';
 import arrowDown from '../icons/arrow-down.svg';
 import arrowUp from '../icons/arrow-up.svg';
 import { useOutsideClick } from '@/shared/lib';
-// TODO need to finish dropdown position adaptive!!!!
+
 export const Header = () => {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
   const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
   const toggleBurgerMenu = () => setIsOpenBurgerMenu((prevState) => !prevState);
   const toggleDropdown = () => setIsOpenDropdown((prevState) => !prevState);
-  const close = useCallback(() => setIsOpenDropdown(false), []);
-  const ref = useOutsideClick<HTMLDivElement>(isOpenDropdown, () => close());
+  const ref = useOutsideClick<HTMLButtonElement>(isOpenDropdown, () =>
+    setIsOpenDropdown(false),
+  );
 
   return (
     <header className="h-20 bg-black flex items-center justify-between  md:justify-end pr-[30px] pt-5 pb-5 pl-[30px]">
-      <div
+      <button
         className="md:hidden flex cursor-pointer flex-col items-center gap-1.5"
         onClick={toggleBurgerMenu}
       >
-        <div
+        <span
           className={`w-[35px] h-1 bg-white duration-300 ${isOpenBurgerMenu ? 'translate-x-0 translate-y-[10px] -rotate-45' : ''}`}
         />
-        <div
+        <span
           className={`w-[35px] h-1 bg-white duration-300 ${isOpenBurgerMenu ? 'opacity-0' : ''}`}
         />
-        <div
+        <span
           className={`w-[35px] h-1 bg-white duration-300 ${isOpenBurgerMenu ? 'translate-x-0 -translate-y-[10px] rotate-45' : ''}`}
         />
-      </div>
-      <div className="relative border border-white w-[200px] lg:w-auto flex justify-end">
-        <div
-          ref={ref}
-          className="text-white flex items-center gap-3 bg-black  rounded-[42px] p-1"
-          onClick={toggleDropdown}
-        >
-          <img src={avatar} alt="avatar" className="h-[35px] w-[35px] object-cover" />
-          <span className="hidden lg:block">Dima Kuzyberdin</span>
-          <button
-            aria-expanded={isOpenDropdown}
-            aria-label="Toggle user menu"
-            className="hidden lg:flex w-4 h-4  items-center justify-center"
-          >
+      </button>
+      <div className="relative min-w-1/2 text-white flex items-center justify-end bg-black  rounded-[42px] p-1">
+        <button onClick={toggleDropdown} className="flex items-center gap-3" ref={ref}>
+          <span className="h-[35px] w-[35px]">
+            <img src={avatar} alt="avatar" className="w-full h-full object-cover block" />
+          </span>
+          <span className="hidden lg:block">Vladimir Kasperovich</span>
+          <span className="hidden lg:flex w-4 h-4  items-center justify-center">
             <img src={isOpenDropdown ? arrowUp : arrowDown} alt="arrow" />
-          </button>
-          {isOpenDropdown && (
-            <ul
-              className={`
-            absolute bg-black top-[42px] right-[30%] font-medium text-[16px] p-4
+          </span>
+        </button>
+
+        <ul
+          role="menu"
+          className={`
+            absolute bg-black top-full right-0 font-medium text-[16px] p-4
             flex flex-col items-start gap-4 ease-in-out duration-300 transition-all
             ${
               isOpenDropdown
@@ -56,22 +53,20 @@ export const Header = () => {
                 : 'opacity-0 scale-95 pointer-events-none'
             }
              `}
-            >
-              {DROPDOWN_ITEMS.map(({ id, link, text, icon }) => (
-                <li key={id} className="flex items-center gap-2 hover:text-gray-300">
-                  <Link
-                    to={link}
-                    onClick={close}
-                    className="flex items-center gap-2 hover:text-gray-300"
-                  >
-                    <img src={icon} alt="" />
-                    <span>{text}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
+        >
+          {DROPDOWN_ITEMS.map(({ id, link, text, icon }) => (
+            <li key={id} className="flex items-center gap-2 hover:text-gray-300">
+              <Link
+                to={link}
+                onClick={() => setIsOpenDropdown(false)}
+                className="flex items-center gap-2 hover:text-gray-300"
+              >
+                <img src={icon} alt={`dropdown ${text}`} />
+                <span>{text}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </header>
   );
