10f3e393b1ebb85a52aba34d37f88a63397f7689
vovak <vova.kasper.boy@gmail.com>
2026-03-17 15:12:08 +0300

    feat: add validation with zod resolver

--- src/pages/createPlaylist/ui/CreatePlaylist.tsx
+++ src/pages/createPlaylist/ui/CreatePlaylist.tsx
@@ -5,11 +5,23 @@ import {
 } from '@/entities/playlists';
 import { toast } from 'react-toastify';
 import media from '../icons/media.svg';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
 
+const FormSchema = z.object({
+  title: z.string().min(3),
+  description: z.string().min(3),
+});
+type FormValues = z.infer<typeof FormSchema>;
 const CreatePlaylist = () => {
   const [createPlaylist] = useCreatePlaylistMutation();
-  const { register, handleSubmit } = useForm<PlaylistCreationData>({
-    defaultValues: { title: '', description: '' },
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+    mode: 'onSubmit',
   });
 
   const submit: SubmitHandler<PlaylistCreationData> = (data) => {
@@ -38,25 +50,31 @@ const CreatePlaylist = () => {
           </button>
         </div>
 
-        <label htmlFor="title" className="flex w-1/2 flex-col gap-1 text-[#808080]">
-          Playlist title
-          <input
-            type="text"
-            id="title"
-            placeholder="Playlist title"
-            className="h-10 rounded-sm border border-[#B3B3B3] bg-[#2F2F2F] p-2 text-white placeholder-[#FFFFFF]"
-            {...register('title')}
-          />
-        </label>
-        <label htmlFor="description" className="flex w-1/2 flex-col gap-1 text-[#808080]">
-          Playlist description
-          <textarea
-            placeholder="Playlist description"
-            id="description"
-            className="h-17.5 rounded-sm border border-[#B3B3B3] bg-[#2F2F2F] p-2 text-white placeholder-[#FFFFFF]"
-            {...register('description')}
-          />
-        </label>
+        <div className="w-1/2">
+          <label htmlFor="title" className="flex flex-col gap-1 text-[#808080]">
+            Playlist title
+            <input
+              type="text"
+              id="title"
+              placeholder="Playlist title"
+              className="h-10 rounded-sm border border-[#B3B3B3] bg-[#2F2F2F] p-2 text-white placeholder-[#FFFFFF]"
+              {...register('title')}
+            />
+          </label>
+          {errors.title && <p>{errors.title.message}</p>}
+        </div>
+        <div className="w-1/2">
+          <label htmlFor="description" className="flex flex-col gap-1 text-[#808080]">
+            Playlist description
+            <textarea
+              placeholder="Playlist description"
+              id="description"
+              className="h-17.5 rounded-sm border border-[#B3B3B3] bg-[#2F2F2F] p-2 text-white placeholder-[#FFFFFF]"
+              {...register('description')}
+            />
+          </label>
+          {errors.description && <p>{errors.description.message}</p>}
+        </div>
         <button type="submit" className="h-10 w-1/2 rounded-sm bg-black">
           CreatePlaylist
         </button>
